f5ba52646ab6fd4f4005df039fa5d6c1b7d84a98
Aleksi <advelex@hotmail.com>
2015-05-02 12:44:44 +0300

    EventManager is now event_manager.py module

--- gui.py
+++ gui.py
@@ -2,104 +2,11 @@ import math
 import pygame
 import pdb
 import time
+import event_manager
+
 import os, pathlib
-
-#Tällä saadaan tietää mikä käyttöjärjestelmä on tällä hetkellä.
 from sys import platform as _platform
-
 from vector2 import Vector2
-
-'''
-Aiemmin Gui luokalla oli metodi: event_manager. Luokka EventManager vastaa sitä,
-sillä erotuksella, että uusi luokka kykenee muistamaan mitkä näppäimet ovat 
-painettuina ja mitkä ei.
-
-Aiemmassa event_manager metodissa playerin kiihtyvyyttä säädettiin suoraan,
-kun tiettyä näppäinta oli painettu. Uusi EventManager pitää erikseen kirjaa
-kaikista (halutuista) näppäimista, mikä selittaa uudet if-else kohdat.
-'''
-class EventManager( object ):
-    
-    '''
-    Initti ottaa parametrin player, jotta se voisi muuttaa tämän kiihtyvyyttä.
-    '''
-    def __init__( self, player ):
-    
-        self.key_up = False
-        self.key_down = False
-        self.key_left = False
-        self.key_right = False
-        
-        self.player = player
-        
-    
-    def CheckInput( self ):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    if self.key_up:
-                        pass
-                    else:
-                        self.player.a.y -= self.player.acceleration
-                        self.key_up = True
-                        
-                if event.key == pygame.K_DOWN:
-                    if self.key_down:
-                        pass
-                    else:
-                        self.player.a.y += self.player.acceleration
-                        self.key_down = True
-                        
-                if event.key == pygame.K_LEFT:
-                    if self.key_left:
-                        pass
-                    else:
-                        self.player.a.x -= self.player.acceleration
-                        self.key_left = True
-                    
-                if event.key == pygame.K_RIGHT:
-                    if self.key_right:
-                        pass
-                    else:
-                        self.player.a.x += self.player.acceleration
-                        self.key_right = True
-
-                if event.key == pygame.K_q:
-                    return False
-
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    if self.key_up:
-                        self.player.a.y += self.player.acceleration
-                        self.key_up = False
-                    else:
-                        pass
-                        
-                if event.key == pygame.K_DOWN:
-                    if self.key_down:
-                        self.player.a.y -= self.player.acceleration
-                        self.key_down = False
-                    else:
-                        pass
-                        
-                if event.key == pygame.K_LEFT:
-                    if self.key_left:
-                        self.player.a.x += self.player.acceleration
-                        self.key_left = False
-                    else:
-                        pass
-                        
-                if event.key == pygame.K_RIGHT:
-                    if self.key_right:
-                        self.player.a.x -= self.player.acceleration
-                        self.key_right = False
-                    else:
-                        pass
-        return True
-
 
 '''
 class Vector:
@@ -157,7 +64,7 @@ class Player(pygame.sprite.Sprite):
 
         self.last_time = time.perf_counter()
         
-        self.event_manager = EventManager( self )
+        self.event_manager = event_manager.EventManager( self )
 
 
     def rot_center(self, image, angle):

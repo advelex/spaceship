32b05c7e04413647d89ac62525dc21ba2a7a4477
Aleksi <advelex@hotmail.com>
2015-05-02 15:40:34 +0300

    Updated event_manager.py style

--- event_manager.py
+++ event_manager.py
@@ -2,22 +2,21 @@ import pygame
 import vector2
 
 
-class EventManager( object ):
-    
+class EventManager(object):
+
     '''
     Initti ottaa parametrin player, jotta se voisi muuttaa tämän kiihtyvyyttä.
     '''
-    def __init__( self, player ):
-    
+    def __init__(self, player):
+
         self.key_up = False
         self.key_down = False
         self.key_left = False
         self.key_right = False
-        
+
         self.player = player
-        
-    
-    def CheckInput( self ):
+
+    def CheckInput(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
@@ -29,21 +28,21 @@ class EventManager( object ):
                     else:
                         self.player.a.y -= self.player.acceleration
                         self.key_up = True
-                        
+
                 if event.key == pygame.K_DOWN:
                     if self.key_down:
                         pass
                     else:
                         self.player.a.y += self.player.acceleration
                         self.key_down = True
-                        
+
                 if event.key == pygame.K_LEFT:
                     if self.key_left:
                         pass
                     else:
                         self.player.a.x -= self.player.acceleration
                         self.key_left = True
-                    
+
                 if event.key == pygame.K_RIGHT:
                     if self.key_right:
                         pass
@@ -61,21 +60,21 @@ class EventManager( object ):
                         self.key_up = False
                     else:
                         pass
-                        
+
                 if event.key == pygame.K_DOWN:
                     if self.key_down:
                         self.player.a.y -= self.player.acceleration
                         self.key_down = False
                     else:
                         pass
-                        
+
                 if event.key == pygame.K_LEFT:
                     if self.key_left:
                         self.player.a.x += self.player.acceleration
                         self.key_left = False
                     else:
                         pass
-                        
+
                 if event.key == pygame.K_RIGHT:
                     if self.key_right:
                         self.player.a.x -= self.player.acceleration

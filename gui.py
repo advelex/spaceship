15adfb63e98780c7ddbe30e93f2baab9c3f75f6d
swrni <swrni.1@gmail.com>
2015-05-03 13:36:34 +0300

    Merged animations to master. Renamed EventManager to InputManager

--- gui.py
+++ gui.py
@@ -1,110 +1,25 @@
-import math
 import pygame
-import pdb
-import time
-import os, pathlib
+import os
+import pathlib
+import sys
 
-import window
-import animations
-
+from window import Window
+from animations import AnimationComponent
 from physics import PhysicsComponent
-
-#Tällä saadaan tietää mikä käyttöjärjestelmä on tällä hetkellä.
-from sys import platform as _platform
-
+from input_manager import InputManager
 from vector2 import Vector2
 
 
-class EventManager( object ):
-
-    def __init__( self, physics_component ):
+class Player( object ):
     
-        self.key_up = False
-        self.key_down = False
-        self.key_left = False
-        self.key_right = False
-        
-        self.physics_component = physics_component
-    
-    def CheckInput( self ):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    if self.key_up:
-                        pass
-                    else:
-                        self.physics_component.acceleration.y -= self.physics_component.acceleration_factor
-                        self.key_up = True
-                        
-                if event.key == pygame.K_DOWN:
-                    if self.key_down:
-                        pass
-                    else:
-                        self.physics_component.acceleration.y += self.physics_component.acceleration_factor
-                        self.key_down = True
-                        
-                if event.key == pygame.K_LEFT:
-                    if self.key_left:
-                        pass
-                    else:
-                        self.physics_component.acceleration.x -= self.physics_component.acceleration_factor
-                        self.key_left = True
-                    
-                if event.key == pygame.K_RIGHT:
-                    if self.key_right:
-                        pass
-                    else:
-                        self.physics_component.acceleration.x += self.physics_component.acceleration_factor
-                        self.key_right = True
-
-                if event.key == pygame.K_q:
-                    return False
-
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    if self.key_up:
-                        self.physics_component.acceleration.y += self.physics_component.acceleration_factor
-                        self.key_up = False
-                    else:
-                        pass
-                        
-                if event.key == pygame.K_DOWN:
-                    if self.key_down:
-                        self.physics_component.acceleration.y -= self.physics_component.acceleration_factor
-                        self.key_down = False
-                    else:
-                        pass
-                        
-                if event.key == pygame.K_LEFT:
-                    if self.key_left:
-                        self.physics_component.acceleration.x += self.physics_component.acceleration_factor
-                        self.key_left = False
-                    else:
-                        pass
-                        
-                if event.key == pygame.K_RIGHT:
-                    if self.key_right:
-                        self.physics_component.acceleration.x -= self.physics_component.acceleration_factor
-                        self.key_right = False
-                    else:
-                        pass
-        return True
-
-
-class Player():
-
-    
-    def __init__( self, acceleration_factor ):
+    def __init__( self, acceleration_factor = None, max_speed = None ):
     
         super().__init__()
         
         m_img_path = ""
         s_img_path = ""
         
-        if _platform == "linux" or _platform == "linux2":
+        if sys.platform == "linux" or sys.platform == "linux2":
             m_img_path = str( pathlib.Path('ship_m.png').resolve() )
             s_img_path = str( pathlib.Path('ship_s.png').resolve() )
         else:
@@ -115,29 +30,28 @@ class Player():
             
         start_pos = Vector2( 150, 150 )
         
-        self.physics_component = PhysicsComponent( start_pos, acceleration_factor )
+        self.physics_component = PhysicsComponent( start_pos, acceleration_factor, max_speed )
         
         image_path = m_img_path
-        self.graphics = animations.AnimationComponent( image_path, self.physics_component )
+        self.graphics = AnimationComponent( image_path, self.physics_component )
         
         self.physics_component.SetGraphicsComponent( self.graphics )
         
-        self.event_manager = EventManager( self.physics_component )
+        self.input_manager = InputManager( self.physics_component )
 
 
-
-class Gui:
-
+class Gui( object ):
 
     def __init__(self):
 
         pygame.init()
                 
         self.window_size = [ 1000, 750 ]
-        self.window = window.Window( self.window_size )
+        self.window = Window( self.window_size )
 
-        acceleration_factor = 0.5
-        self.ship = Player( acceleration_factor )
+        acceleration_factor = 2.5
+        max_speed = 8
+        self.ship = Player( acceleration_factor, max_speed )
         
         self.fps = 60
         
@@ -147,7 +61,7 @@ class Gui:
 
     def HandleInput(self):
     
-        return self.ship.event_manager.CheckInput()
+        return self.ship.input_manager.CheckInput()
 
 
     def loop(self):
@@ -163,7 +77,6 @@ class Gui:
             #self.clock.tick( 60*1000 )
 
         pygame.quit()
-
 
 
 def main():
--- vector2.py
+++ vector2.py
@@ -1,159 +1,159 @@
- 
+
 from math import ( hypot, degrees, atan2, cos, sin, radians )
 
 
 class Vector2( object ):
-        '''
-        Vector2 class
-        '''
+    '''
+    Vector2 class
+    '''
 
-        def __init__( self, x = None, y = None ):
-                
-                self.x = x
-                self.y = y
-                self.r = self.GetR()
-                self.fii = self.GetAngle()
-                
-                
-        def GetAngle( self ):
-        
-                return degrees( atan2( self.x, self.y ) )
-                
+    def __init__( self, x = None, y = None ):
+            
+        self.x = x
+        self.y = y
+        self.r = self.GetR()
+        self.fii = self.GetAngle()
+            
+            
+    def GetAngle( self ):
 
-        def GetR( self ):
-        
-                return hypot( self.x, self.y )
-                
+        return degrees( atan2( self.x, self.y ) )
+            
 
-        def UpdatePolar( self ):
-        
-                self.r = self.GetR()
-                self.fii = self.GetAngle()
-                
+    def GetR( self ):
 
-        def UpdateCartesian( self ):
-        
-                self.x = self.r * cos( radians( self.fii ) )
-                self.y = self.r * sin( radians( self.fii ) )
+        return hypot( self.x, self.y )
+            
+
+    def UpdatePolar( self ):
+
+        self.r = self.GetR()
+        self.fii = self.GetAngle()
+            
+
+    def UpdateCartesian( self ):
+
+        self.x = self.r * cos( radians( self.fii ) )
+        self.y = self.r * sin( radians( self.fii ) )
 
 
-        def Magnitude( self ):
+    def Magnitude( self ):
+
+        return hypot( self.x, self.y )
+            
+            
+    def Normalize( self ):
         
-                return hypot( self.x, self.y )
-                
-                
-        def Normalize( self ):
-                
-                return Vector2( self.x, self.y ) * ( 1.0 / self.Magnitude() )
-                
-                
-        def Clamp( self, min, max ):
+        return Vector2( self.x, self.y ) * ( 1.0 / self.Magnitude() )
+            
+            
+    def Clamp( self, min, max ):
+
+        x = 0
+        y = 0
         
-                x = 0
-                y = 0
+        if ( min is not None ):
+                x = float( Clamp( self.x, min, None ) )
+                y = float( Clamp( self.y, min, None ) )
+        if ( max is not None ):
+                x = float( Clamp( self.x, None, max ) )
+                y = float( Clamp( self.y, None, max ) )
+        
+        return Vector2( x, y )
+            
+            
+    def ClampMagnitude( self, max_magnitude ):
+
+        mag = self.Magnitude()
+        
+        if ( mag > max_magnitude ):
+                # print( "mag:{}." .format( mag ) )
+                # print( "max_magnitude:{}." .format( max_magnitude ) )
+                # print( "old vector:{}." .format( self ) )
+                # print( "new vector:{}." .format( self.Normalize() * max_magnitude ) )
+                return self.Normalize() * max_magnitude
                 
-                if ( min is not None ):
-                        x = float( Clamp( self.x, min, None ) )
-                        y = float( Clamp( self.y, min, None ) )
-                if ( max is not None ):
-                        x = float( Clamp( self.x, None, max ) )
-                        y = float( Clamp( self.y, None, max ) )
+        return self
+            
+                    
+    def __str__( self ):
+
+        return "<" + str( self.x ) + ", " + str( self.y ) + ">"
+
+
+    # Returns a new Vector2
+    def __sub__( self, other ):
+
+        if ( other is None ):
+                raise Exception("\nparameter is None\n")
+        
+        try:
+                x = float( self.x - other.x )
+                y = float( self.y - other.y )
                 
                 return Vector2( x, y )
                 
-                
-        def ClampMagnitude( self, max_magnitude ):
-        
-                mag = self.Magnitude()
-                
-                if ( mag > max_magnitude ):
-                        # print( "mag:{}." .format( mag ) )
-                        # print( "max_magnitude:{}." .format( max_magnitude ) )
-                        # print( "old vector:{}." .format( self ) )
-                        # print( "new vector:{}." .format( self.Normalize() * max_magnitude ) )
-                        return self.Normalize() * max_magnitude
-                        
-                return self
-                
-                        
-        def __str__( self ):
-        
-                return "<" + str( self.x ) + ", " + str( self.y ) + ">"
+        except TypeError:
+                raise TypeError
+                    
+    
+    # Returns a new Vector2
+    def __add__( self, other ):
 
-
-        # Returns a new Vector2
-        def __sub__( self, other ):
+        if ( other is None ):
+                raise Exception("\nparameter is None\n")
         
-                if ( other is None ):
-                        raise Exception("\nparameter is None\n")
+        try:
+                x = float( self.x + other.x )
+                y = float( self.y + other.y )
                 
-                try:
-                        x = float( self.x - other.x )
-                        y = float( self.y - other.y )
-                        
-                        return Vector2( x, y )
-                        
-                except TypeError:
-                        raise TypeError
-                        
+                return Vector2( x, y )
+                
+        except TypeError:
+                raise TypeError
+                    
+            
+    # Returns a new Vector2    
+    def __mul__( self, other ):
+    
+        if ( other is None ):
+                raise Exception("\nparameter is None\n")
         
-        # Returns a new Vector2
-        def __add__( self, other ):
-        
-                if ( other is None ):
-                        raise Exception("\nparameter is None\n")
+        try:
+                x = self.x * other
+                y = self.y * other
                 
-                try:
-                        x = float( self.x + other.x )
-                        y = float( self.y + other.y )
-                        
-                        return Vector2( x, y )
-                        
-                except TypeError:
-                        raise TypeError
-                        
+                return Vector2( x, y )
                 
-        # Returns a new Vector2    
-        def __mul__( self, other ):
-        
-                if ( other is None ):
-                        raise Exception("\nparameter is None\n")
-                
-                try:
-                        x = self.x * other
-                        y = self.y * other
-                        
-                        return Vector2( x, y )
-                        
-                except TypeError:
-                        raise TypeError
-                        
+        except TypeError:
+                raise TypeError
+                    
 
 def main():
-        v = Vector2( 0, 0 )
-        v.GetAngle()
-        v.GetR()
-        v.UpdatePolar()
-        v.UpdateCartesian()
-        
-        
+    v = Vector2( 0, 0 )
+    v.GetAngle()
+    v.GetR()
+    v.UpdatePolar()
+    v.UpdateCartesian()
+    
+    
 if ( __name__ == '__main__' ):
-        main()
-                        
-                        
-                        
-              
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
-                        
+    main()
+                    
+                    
+                    
+          
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
